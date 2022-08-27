8b2176132266856d59c19f9028917495b6c54e4e
Simon Reinhardt <saadabou.study@gmail.com>
2022-08-27 19:44:21 +0100

    we design tomorrow

--- app/js/script.js
+++ app/js/script.js
@@ -570,3 +570,34 @@ function PlanetAdvanced(){
 
 
 PlanetAdvanced();
+
+
+
+
+/// three 
+
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, canvas.innerWidth / canvas.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(canvas.innerWidth, canvas.innerHeight);
+canvas.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+};
+
+animate();
